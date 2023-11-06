7365a2e21dd91b3443f50a8c29f734d27c59e64a
sohanr <mizan.rahman66d@gmail.com>
2023-11-06 19:02:53 +0600

    2665

---
+++ 30 days of Javascript/Closures/2665. Counter II.js
@@ -0,0 +1,19 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let n = init
+    return {
+        increment:() => n +=1,
+        decrement:() => n-=1,
+        reset:() => n = init
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
